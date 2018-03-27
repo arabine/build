6c6660cbe618f40df38510864fb4b2ed3e4fc1d6
Anthony Rabine <anthony@rabine.fr>
2018-03-27 21:17:27 +0200

    Added cross compiler support for GCC

--- Main.mk
+++ Main.mk
@@ -9,45 +9,75 @@
 # this turns off the suffix rules built into make
 .SUFFIXES:
 
-# *******************************************************************************
-# OS DETECTION
-# *******************************************************************************
-OS_DETECTED=
+TARGET_ARCH :=
+TARGET_SUFFIX := 
 
-ifeq ($(OS),Windows_NT)
-    OS_DETECTED := WIN32
-else
-    UNAME_S := $(shell uname -s)
-    ifeq ($(UNAME_S),Linux)
-        OS_DETECTED := LINUX
-    endif
-endif
-
+# *******************************************************************************
+# COMPILER DETECTION
+# *******************************************************************************
 ifeq (gcc, $(findstring gcc, $(ARCH)))
 # Compiler setting
-CC      = gcc
-CPP		= g++
-AR      = ar
-AS      = as
-LD 		= g++
-LDFLAGS = $(addprefix -L, $(APP_LIBPATH))
+CC      = $(GCC_PREFIX)gcc
+CPP		= $(GCC_PREFIX)g++
+AR      = $(GCC_PREFIX)ar
+AS      = $(GCC_PREFIX)gcc -x assembler-with-cpp
+LD 		= $(GCC_PREFIX)g++
+SZ 		= $(GCC_PREFIX)size
+CP		= $(PREFIX)objcopy
+HEX		= $(CP) -O ihex
+BIN		= $(CP) -O binary -S
+LIBDIR 	= $(addprefix -L, $(APP_LIBPATH))
 
 # FIXME: different CFLAGS for debug/release targets
-# DEFINES	+= -DUNICODE -DCONFIG_NATIVE_WINDOWS # FIXME why ?
-CFLAGS  = -c -pipe -g -O0 -pedantic -std=c99 -ggdb -Wall -Wextra
-CPPFLAGS = -c -pipe -g -O0 -pedantic -ggdb -Wall -Wextra -std=c++11
 ARFLAGS = rcs
 
-ifeq ($(OS_DETECTED), WIN32)
-CFLAGS  += -fno-keep-inline-dllexport
-DEFINES	+= -DUSE_WINDOWS_OS
-LDFLAGS += -Wl,-subsystem,console -lws2_32 -lpsapi -lwinmm -static-libgcc -static-libstdc++ -static -lpthread
-else
+ifeq (cm4, $(findstring cm4, $(ARCH)))
 
-DEFINES	+= -DUSE_UNIX_OS
-LDFLAGS += -ldl -lpthread
+OPT 			:= -Og
+CFLAGS 			:= -c $(OPT) -gdwarf-2 -Wall -fdata-sections -ffunction-sections
+TARGET_ARCH 	:= Cortex-M4
+CPU 			:= -mcpu=cortex-m4
+TARGET_SUFFIX 	:= .elf
+MCU 			:= $(CPU) -mfpu=fpv4-sp-d16 -mthumb -mfloat-abi=hard
+CFLAGS 			+= $(MCU)
+CPPFLAGS 		+= $(MCU)
 
-endif
+# libraries
+LIBS 			= -lc -lm -lnosys 
+LDFLAGS 		= $(MCU) -specs=nano.specs -T$(APP_LDSCRIPT) $(LIBDIR) $(LIBS) -Wl,-Map=$(OUTDIR)/$(PROJECT).map,--cref -Wl,--gc-sections
+
+# Dependency flags
+CFLAGS += -MMD -MP
+
+endif # Cortex-M4
+
+# *******************************************************************************
+# HOST ARCHITECTURE: Unix, Windows, MacOS X
+# *******************************************************************************
+ifeq (host, $(findstring host, $(ARCH)))
+
+    ifeq ($(OS),Windows_NT)
+        TARGET_ARCH := WIN32
+    else
+        UNAME_S := $(shell uname -s)
+        ifeq ($(UNAME_S),Linux)
+            TARGET_ARCH := LINUX
+        endif
+    endif
+
+CFLAGS  = -c -pipe -g -O0 -pedantic -std=c99 -ggdb -Wall -Wextra
+CPPFLAGS = -c -pipe -g -O0 -pedantic -ggdb -Wall -Wextra -std=c++11
+
+    ifeq ($(TARGET_ARCH), WIN32)
+        CFLAGS  += -fno-keep-inline-dllexport
+        DEFINES	+= -DUSE_WINDOWS_OS
+        LDFLAGS += -Wl,-subsystem,console -lws2_32 -lpsapi -lwinmm -static-libgcc -static-libstdc++ -static -lpthread
+    else
+        DEFINES	+= -DUSE_UNIX_OS
+        LDFLAGS += -ldl -lpthread
+    endif
+endif # endif host architecture
+
 
 endif # GCC
 
@@ -82,7 +112,7 @@ endef
 
 
 SOURCES :=
-
+INCLUDES :=
 
 # Include all the modules sub-makefiles in one command
 -include $(patsubst %, %/Module.mk, $(ALL_MODULES))
@@ -91,52 +121,57 @@ SOURCES :=
 # Deduct objects to build 
 OBJECTS := $(addprefix $(OUTDIR),$(patsubst %.c, %.o, $(filter %.c,$(SOURCES))))
 OBJECTS += $(addprefix $(OUTDIR),$(patsubst %.cpp, %.o, $(filter %.cpp,$(SOURCES))))
+OBJECTS += $(addprefix $(OUTDIR),$(patsubst %.s, %.o, $(filter %.s,$(SOURCES))))
 
+
+DEPENDENCIES := $(patsubst %.o,%.d,$(OBJECTS))
 
 # Include generated dependency files, if any
-ifeq ($(ENABLE_DEP), true)
-	# List of dependencies
-	DEPENDENCIES := $(patsubst %.o,%.d,$(OBJECTS))
-	# Dependency flags
-	DEPEND_FLAGS = -MMD
-endif
+#-include $(DEPENDENCIES)
 
-INCLUDES_CPY:=$(INCLUDES)
-
--include $(DEPENDENCIES)
-
-INCLUDES_CPY += $(ALL_MODULES)
+INCLUDES += $(ALL_MODULES)
 
 $(addprefix $(OUTDIR), %.o): %.c
 	@echo "Building file: $(notdir $@)"
 	$(VERBOSE) $(MKDIR) "$(dir $@)"
-	$(VERBOSE) $(CC) $(CFLAGS) $(DEFINES) $(addprefix -I, $(INCLUDES_CPY)) $(DEPEND_FLAGS) -o $@ $<
+	$(VERBOSE) $(CC) $(CFLAGS) $(DEFINES) $(addprefix -I, $(INCLUDES)) $(DEPEND_FLAGS) -o $@ $<
 
 $(addprefix $(OUTDIR), %.o): %.cpp
 	@echo "Building file: $(notdir $@)"
 	$(VERBOSE) $(MKDIR) "$(dir $@)"
-	$(VERBOSE) $(CPP) $(CPPFLAGS) $(DEFINES) $(addprefix -I, $(INCLUDES_CPY)) $(DEPEND_FLAGS) -o $@ $<
+	$(VERBOSE) $(CPP) $(CPPFLAGS) $(DEFINES) $(addprefix -I, $(INCLUDES)) $(DEPEND_FLAGS) -o $@ $<
 
 $(addprefix $(OUTDIR), %.o): %.s
 	@echo "Building file: $(notdir $@)"
 	$(VERBOSE) $(MKDIR) "$(dir $@)"
-	$(VERBOSE) $(AS) $(ASFLAGS) -o $@ $< 
-	
+	$(VERBOSE) $(AS) $(CFLAGS) -o $@ $<
+
+$(OUTDIR)$(PROJECT).hex: $(OUTDIR)$(PROJECT).elf | $(OUTDIR)
+	$(HEX) $< $@
+
+
 # *******************************************************************************
 # GENERIC
 # *******************************************************************************
 
-# Arguments: $1=objects $2=libs $3=executable name
-define linker
-	@echo "Invoking: Linker $(OS_DETECTED)"
-	$(VERBOSE) $(LD) $(1) $(2) $(LDFLAGS) -o $(OUTDIR)$(strip $(3))
-	@echo "Finished building target: $(strip $(3))"
+TARGET_NAME	:= $(PROJECT)$(TARGET_SUFFIX)
+EXECUTABLE 	:= $(OUTDIR)$(TARGET_NAME)
+
+.PHONY: $(EXECUTABLE)
+$(EXECUTABLE): $(OBJECTS)
+	@echo "Invoking: Linker $(TARGET_ARCH)"
+	$(CC) $(OBJECTS) $(APP_LIBS) $(LDFLAGS) -o $(EXECUTABLE)
+	@echo "Finished building target: $(TARGET_NAME)"
 	@echo " "
-endef
+	$(SZ) $@
+
+.PHONY: all
+all: $(EXECUTABLE) $(OUTDIR)$(PROJECT).hex
+
 
 # Arguments: $1=objects $2=library name
 define librarian
-	@echo "Invoking: Librarian $(OS_DETECTED)"
+	@echo "Invoking: Librarian $(TARGET_ARCH)"
 	$(VERBOSE) $(AR) $(ARFLAGS) $(OUTDIR)$(strip $(2) $(1))
 	@echo "Finished building library: $(strip $(2))"
 	@echo " "
